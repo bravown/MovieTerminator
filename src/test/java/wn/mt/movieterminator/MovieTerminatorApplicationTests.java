0f834e15bfc5407b9f88a7780678e884f8641aae
wangning <bravown@163.com>
2023-05-05 09:51:20 +0800

    初次提交

---
+++ src/test/java/wn/mt/movieterminator/MovieTerminatorApplicationTests.java
@@ -0,0 +1,13 @@
+package wn.mt.movieterminator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTerminatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
